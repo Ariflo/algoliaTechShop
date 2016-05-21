a4ab2310f23bad18bef81149f37203d8a79156b4
AriFlo <ariflo@arianflores.com>
2016-05-21 16:47:00 -0700

    added gulp plug-ins

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,64 @@
 var gulp = require('gulp');
+
+/**
+*Gulp plug-ins
+*/
+//mocha for testing 
 var mocha = require('gulp-mocha');
+//util to log errors
 var util = require('gulp-util');
+//linter to verify js files
+var jshint = require('gulp-jshint');
+//to compile sass files
+var sass = require('gulp-sass');
+//concatnates JS files
+var concat = require('gulp-concat');
+//minitfy JS files
+var uglify = require('gulp-uglify');
+//rename files
+var rename = require('gulp-rename');
 
 /**
  * Gulp Tasks
  */
 
+ // Lint Task
+ gulp.task('lint', function() {
+     return gulp.src('/public/js/*.js')
+         .pipe(jshint())
+         .pipe(jshint.reporter('default'));
+ });
+
+ // Compile Our Sass
+ gulp.task('sass', function() {
+     return gulp.src('/public/scss/*.scss')
+         .pipe(sass())
+         .pipe(gulp.dest('dist/css'));
+ });
+
+ // Concatenate & Minify JS
+ gulp.task('scripts', function() {
+     return gulp.src('public/js/*.js')
+         .pipe(concat('all.js'))
+         .pipe(gulp.dest('dist'))
+         .pipe(rename('all.min.js'))
+         .pipe(uglify())
+         .pipe(gulp.dest('dist/js'));
+ });
+
+ // Watch Files For Changes
+ gulp.task('watch', function() {
+     gulp.watch('/public/js/*.js', ['lint', 'scripts']);
+     gulp.watch('scss/*.scss', ['sass']);
+     gulp.watch(['views/**', 'public/**', '.js', 'routes/**', 'test/**'], ['test']);
+ });
+
+//Run tests
  gulp.task('test', function () {
      return gulp.src(['test/**/*.js'], { read: false })
          .pipe(mocha({ reporter: 'spec' }))
          .on('error', util.log);
  });
 
- gulp.task('watch-test', function () {
-     gulp.watch(['views/**', 'public/**', 'app.js', 'routes/**', 'test/**'], ['test']);
- });
-
  // *** default task *** //
- gulp.task('default', ['test', 'watch-test'], function(){});
+ gulp.task('default', ['test', 'lint', 'sass', 'scripts', 'watch'], function(){});
