6786e5110c34993ac1974097aea2a95d99a75235
AriFlo <ariflo@arianflores.com>
2016-05-25 10:15:46 -0700

    modal set up

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,30 +1,36 @@
 algoliaApp.controller('homeController', ['$scope', '$http', '$parse', '$location', '$routeParams', '$timeout', '$anchorScroll', '$window', 'anchorSmoothScroll', 'algolia'
 	                                     ,function($scope,  $http,  $parse,  $location,   $routeParams, $timeout, $anchorScroll,$window, anchorSmoothScroll, algolia) {
 
+	            //hide modal form         
+	            $scope.showProduct = false;                         		
+
+	            //initialize search, hits, and product
 	             $scope.query = ' ';
-	             $scope.hits = [];           
+	             $scope.hits = [];   
+	             $scope.product = {};
 
 		  var client = algolia.Client('W8I2YD0GJC', '861d0757703675d68f5a0f915072381b');
 		  var index = client.initIndex('tech_shop_data');
 		  index.clearCache();
 
-		  $scope.algoliaSearch = function (){
-		  	index.search($scope.query, {hitsPerPage: 10000})
-		  	.then(function searchSuccess(content) {
-		  	     	$scope.products = content.hits;
-
-		  	     	$scope.product.url = $scope.products.url;
-		  	     	$scope.product.image = $scope.products.image;
-		  	     	$scope.product.type = $scope.products.type;
-		  	     	$scope.product.name = $scope.products.name;
-		  	     	$scope.product.price = $scope.products.price;
-		  	     	$scope.product.description =  $scope.products.description;
-		  	     	$scope.product.type = $scope.products.type;
- 
-
-		  	}, function searchFailure(err) {
-		  	     	console.log(err);
-		  	})
+		  $scope.algoliaSearch = function (product){
+		  	if(product){
+		  		$scope.showProduct = !$scope.showProduct;
+		  		$scope.product.url = product.url;
+		  		$scope.product.image = product.image;
+		  		$scope.product.type = product.type;
+		  		$scope.product.name = product.name;
+		  		$scope.product.price = product.price;
+		  		$scope.product.description = product.description;
+		  		$scope.product.type = product.type;
+		  	}else{
+		  		index.search($scope.query, {hitsPerPage: 10000})
+		  		.then(function searchSuccess(content) {
+		  		     	$scope.products = content.hits;
+		  		}, function searchFailure(err) {
+		  		     	console.log(err);
+		  		})
+		  	}
 		 }
 		 $scope.algoliaSearch();
 }]);
